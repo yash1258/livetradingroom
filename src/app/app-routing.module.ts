170fd594cc22487906a46066e1543c0bdf55c8f4
Yash raj <yraj0770@gmail.com>
2020-11-26 19:05:05 +0530

    dividing components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'navbar-component', component: NavbarComponent
+  },
+  {
+    path: 'login-component', component: LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {animate, style, transition, trigger} from "@angular/animations";
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'livetradingroom';
+
+
+  constructor() {};
+
+  ngOnInit(): void {};
 }
