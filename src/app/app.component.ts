c170ffa7a804c33b351eb90d7e80db0f4fc5e39b
Arshad <arshadpakkali@gmail.com>
2020-11-30 08:49:17 +0530

    Checkout ui and logout/ is auth implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,15 @@
 import { Component } from '@angular/core';
-import {animate, style, transition, trigger} from "@angular/animations";
-
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'livetradingroom';
 
+  constructor() {}
 
-  constructor() {};
-
-  ngOnInit(): void {};
+  ngOnInit(): void {}
 }
