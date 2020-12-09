3a61ac2dd9c120f1a4e7fabedf3ea8ce96a9933b
Yash raj <yraj0770@gmail.com>
2020-12-09 20:33:01 +0530

    added footer/profile page

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   api_url:'https://livetradingroom.herokuapp.com',
-  razorpay_key: 'rzp_test_J9bCZI7rvK4nYI',
+  razorpay_key: 'rzp_test_J9bCZI7rvK4nYI'
 };
