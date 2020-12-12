7651d6cda5da7302ebee3a54b0d0d3b656466c08
Yash raj <yraj0770@gmail.com>
2020-12-12 21:45:46 +0530

    images/background stash

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -31,19 +31,19 @@ export class DashboardComponent implements OnInit {
     myParams: object = {
       "particles": {
         "number": {
-          "value": 100,
+          "value": 80,
           "density": {
             "enable": true,
             "value_area": 800
           }
         },
         "color": {
-          "value": "#000"
+          "value": "#fff"
         },
         "shape": {
           "type": "circle",
           "polygon": {
-            "nb_sides": 7
+            "nb_sides": 5
           },
           "image": {
             "src": "img/github.svg",
@@ -56,31 +56,21 @@ export class DashboardComponent implements OnInit {
           "random": false,
           "anim": {
             "enable": false,
-            "speed": 1,
-            "opacity_min": 0.1,
-            "sync": false
-          }
-        },
-        "size": {
-          "value": 3,
-          "random": true,
-          "anim": {
-            "enable": false,
             "speed": 40,
-            "size_min": 0.1,
+            "opacity_min": 0.1,
             "sync": false
           }
         },
         "line_linked": {
           "enable": true,
           "distance": 150,
-          "color": "#000",
+          "color": "#fff",
           "opacity": 0.4,
-          "width": 1.5
+          "width": 1
         },
         "move": {
           "enable": true,
-          "speed": 7,
+          "speed": 6,
           "direction": "none",
           "random": false,
           "straight": false,
